64280a64e3ae7d7e6906426c3cfdd6631683f074
sophia22001 <yujii22001@gmail.com>
2025-06-10 00:29:18 +0900

    feat: 추가 / 삭제 함수

--- App.tsx
+++ App.tsx
@@ -1,130 +1,86 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState} from 'react';
+import {View, StyleSheet, Text, Alert, ScrollView} from 'react-native';
+import Header from './src/header';
+import Generator from './src/generator';
+import NumList from './src/numlist';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const stateArr = {
+  appName: 'My First App',
+  random: [36, 999],
+};
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [state, setState] = useState(stateArr);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const onAddRandomNum = () => {
+    const randomNum = Math.floor(Math.random() * 100) + 1;
+    setState(prevState => {
+      return {...prevState, random: [...prevState.random, randomNum]};
+    });
   };
 
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the recommendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
+  const onNumDelete = position => {
+    const newArray = state.random.filter((num, index) => {
+      return position !== index;
+    });
+    setState(prevState => ({
+      ...prevState,
+      random: newArray,
+    }));
+  };
 
   return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header />
-        </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+    <View style={styles.mainView}>
+      <Header name={state.appName} />
+      <View>
+        <Text style={styles.mainText} onPress={() => Alert.alert('helloworld')}>
+          Hello World
+        </Text>
+      </View>
+
+      <Generator add={onAddRandomNum} />
+
+      <ScrollView
+        style={{width: '100%'}}
+        // onMomentumScrollBegin={() => Alert.alert('begin')}
+        // onMomentumScrollEnd={() => Alert.alert('begin')}>
+        // onScroll={() => Alert.alert('Scrolling')}
+
+        //</View>onContentSizeChange={(width, height) => Alert.alert(height.toString())}
+        bounces={false}>
+        <NumList num={state.random} onDelete={onNumDelete} />
       </ScrollView>
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
+
+const styles = StyleSheet.create({
+  mainView: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingTop: 50,
+    alignItems: 'center',
+    //justifyContent: 'center',
+  },
+  subView: {
+    backgroundColor: 'yellow',
+    marginBottom: 10,
+  },
+  anotherSubView: {
+    flex: 2,
+    backgroundColor: 'yellow',
+    marginBottom: 10,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mainText: {
+    fontSize: 20,
+    fontWeight: 'normal',
+    color: 'red',
+    padding: 20,
+  },
+});
