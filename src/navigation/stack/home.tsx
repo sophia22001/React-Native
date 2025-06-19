129aa2f13e03438bac49591a2d7859e4b128d68b
sophia22001 <yujii22001@gmail.com>
2025-06-19 23:41:32 +0900

    feat: 화면 전환

--- src/navigation/stack/home.tsx
+++ src/navigation/stack/home.tsx
@@ -1,11 +1,16 @@
 import {useNavigation} from '@react-navigation/native';
 import React, {useEffect} from 'react';
-import {Text, View} from 'react-native';
+import {Button, Text, View} from 'react-native';
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>home</Text>
+      <Button
+        title="To profile"
+        onPress={() => navigation.navigate('Profile')}
+      />
     </View>
   );
 };
