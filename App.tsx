3336df49b7bc06784143a5c11e186d90c5c1b1dc
sophia22001 <yujii22001@gmail.com>
2025-06-20 00:22:42 +0900

    feat: drawer 처음 틀

--- App.tsx
+++ App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
+import './gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {StyleSheet} from 'react-native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import HomeScreen from './src/navigation/stack/home';
-import UserScreen from './src/navigation/stack/user';
-import ProfileScreen from './src/navigation/stack/profile';
-import CreatePostScreen from './src/navigation/stack/CreatePost';
 import {createDrawerNavigator} from '@react-navigation/drawer';
-const Stack = createNativeStackNavigator();
+
+import HomeDrawer from './src/navigation/drawer/Home';
+import UserDrawer from './src/navigation/drawer/User';
+
 const Drawer = createDrawerNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
       <Drawer.Navigator>
-        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Home" component={HomeDrawer} />
+        <Drawer.Screen name="User" component={UserDrawer} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({});
---
+++ src/navigation/drawer/Home.tsx
@@ -0,0 +1,13 @@
+import {useNavigation} from '@react-navigation/native';
+import React, {useEffect} from 'react';
+import {Button, Text, View} from 'react-native';
+
+const HomeDrawer = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>home</Text>
+    </View>
+  );
+};
+
+export default HomeDrawer;
