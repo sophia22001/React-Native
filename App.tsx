91a2a5bc5b52b3bbd9a07329eb00fe53ded427fd
sophia22001 <yujii22001@gmail.com>
2025-06-20 15:58:49 +0900

    feat: 드로어 네비게이션

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
 import React from 'react';
 import './gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
-import {StyleSheet} from 'react-native';
+import {Image, StyleSheet} from 'react-native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 
 import HomeDrawer from './src/navigation/drawer/Home';
 import UserDrawer from './src/navigation/drawer/User';
+import CustomDrawerContent from './src/navigation/drawer/CustomDrawerContent';
+import HomeIcon from './assets/images/home.png';
+import MyDrawer from './src/navigation/drawer/MyDrawer';
 
 const Drawer = createDrawerNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="Home" component={HomeDrawer} />
+      <Drawer.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          drawerType: 'front',
+          drawerPosition: 'right',
+          drawerStyle: {
+            //backgroundColor: '#c6cbef',
+            width: 200,
+          },
+          drawerActiveTintColor: 'white',
+          drawerActiveBackgroundColor: '#003CB3',
+        }}
+        drawerContent={props => <MyDrawer {...props} />}>
+        <Drawer.Screen
+          name="Home"
+          component={HomeDrawer}
+          options={{
+            drawerIcon: () => (
+              <Image source={HomeIcon} style={{width: 30, height: 30}} />
+            ),
+          }}
+        />
         <Drawer.Screen name="User" component={UserDrawer} />
       </Drawer.Navigator>
     </NavigationContainer>
--- src/navigation/drawer/Home.tsx
+++ src/navigation/drawer/Home.tsx
@@ -3,9 +3,12 @@ import React, {useEffect} from 'react';
 import {Button, Text, View} from 'react-native';
 
 const HomeDrawer = () => {
+  const navigation = useNavigation();
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>home</Text>
+      <Button title="To User" onPress={() => navigation.navigate('User')} />
+      <Button title="To Custon" onPress={() => navigation.navigate('Custom')} />
     </View>
   );
 };
